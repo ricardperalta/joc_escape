54d8f8c257999416d64b2de674405c315a949ff2
rperalta18 <videomatic2015@gmail.com>
2021-11-22 23:25:04 +0100

    On branch master, els buttons es mouen bé i desapareixen al entrar a la zona.  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   escape.js
    	modified:   geom2D.js
    	modified:   index_escape.html

--- escape.js
+++ escape.js
@@ -2,51 +2,67 @@ window.onload = function () {
     // Variables
     var MovRaton=0
   
-    let xPos = 0;
-    let yPos = 0;
+    let points = 0;
 
-    let posicionActual = 0;
-    let $boton = document.querySelector('#boton');
+    let buttArray = [...document.getElementsByClassName("balls")];
     let $mainWin = document.querySelector('#mainWindow')
-    let labelVec = document.querySelector('#vec')
+    let labelPoints = document.querySelector('#points')
     let labelMousePos = document.querySelector('#mPos')
     let labelPrevPos = document.querySelector('#prevPos')
     let labelNextPos = document.querySelector('#nextPos')
+    
+    // init buttons at random position
+    buttArray.forEach(but => {
+        but.style.left = Math.random() * 500 + "px";
+        but.style.top = Math.random() * 500 + "px";
+    });
     // Funciones
    
     /* Cambia la posición del Boton */
    
-    function CambiaPosicion(e){
-        let prevX, prevY, nextX, nextY;
+    function MoveAllButtons(e){
+        let M, B, Vmb, dist;
+
+        // show mouse position in console.
         console.log("mouse location:", e.clientX, e.clientY)
-        MovRaton=(MovRaton+1)%10
+        // Only execute one of each ten times.
+        MovRaton = (MovRaton + 1)%10
         if (MovRaton==0) {
-            // $boton.style.top = event.y - Math.random()*30 - $boton.clientHeight-10
-            // $boton.style.left = event.x - Math.random()*30 - $boton.clientWidth-10
-            prevX = $boton.offsetLeft;
-            prevY = $boton.offsetTop;
-           
-            vx = e.clientX - prevX; 
-            vy = e.clientY - prevY;
+            M = new Point(e.clientX, e.clientY);
+            // all buttons move to scape.
+            buttArray.forEach(but => {
+                B = new Point(but.offsetLeft, but.offsetTop);
+                // create vector from M to B.
+                Vmb = M.crearVect(B);
+                dist = Vmb.dist();
+                if (dist < 200) {
+                    but.style.left = but.offsetLeft + Vmb.x * 1000 / (dist**2); 
+                    but.style.top = but.offsetTop + Vmb.y * 1000/ (dist**2);
+                    if (but.offsetLeft < 0) but.style.left = 50;
+                    else if (but.offsetLeft > 500) but.style.left = 450;
+                    if (but.offsetTop < 0) but.style.top = 50;
+                    else if (but.offsetTop > 500) but.style.top = 450;
+                }
+            });
+            // test if some button is inside
+            let center = new Point(250, 250);
+            let radi = 25;
+            buttArray.forEach(but => {
+                if (but.style.visibility != "hidden") {
+                    P = new Point(but.offsetLeft + but.offsetWidth / 2, but.offsetTop + but.offsetHeight / 2);
+                    if (P.isInside(center, radi)) {
+                        but.style.visibility = "hidden";
+                        points++;
+                    }
+                }
+            });
             
-            if ((prevX == 0 || prevY == 0) && (vx < 100  || vy < 100)) {
-                nextX = e.clientX + parseInt(vx/4);
-                nextY = e.clientY + parseInt(vy/4);
-            }
-            else {
-                nextX = prevX - parseInt(200/vx);
-                nextY = prevY - parseInt(200/vy);
-            }
-            if (nextX < 0) nextX = 0;
-            if(nextY < 0)  nextY = 0; 
-            
-            $boton.style.left = nextX;
-            $boton.style.top = nextY;
-            
-            labelMousePos.innerHTML = "Mouse (" + e.clientX + ", " + e.clientY + ")";
+            labelPoints.innerHTML = "Punts: " + points;
+            /*
             labelVec.innerHTML = "Vec (" + vx + ", " + vy + ")";
             labelNextPos.innerHTML = "Next (" + nextX + ", " + nextY + ")";
             labelPrevPos.innerHTML = "Prev (" + prevX + ", " + prevY + ")";
+            */
         }
     }
 
@@ -60,9 +76,7 @@ window.onload = function () {
 
 
     // Eventos
-   
-    $boton.addEventListener('click', botonPillado);
-    $mainWin.addEventListener('mousemove', CambiaPosicion, true);
+    $mainWin.addEventListener('mousemove', MoveAllButtons, true);
   
     // inicio
 } 
--- geom2D.js
+++ geom2D.js
@@ -4,10 +4,30 @@ class Point {
        this.y = yy;
     }
 
-    // Método
+    // this és el punt origen i p2 és el punt final, 
+    // retorna un vector que va de this a p2.
+    crearVect(p2) {
+      let vRes = new Vec(p2.x - this.x, p2.y - this.y);
+      return vRes;
+    }
+
+    // suma el vector 'v' al punt this,
+    // modifica les coordenades del punt this.
+    sumaVect(v) {
+
+    }
+   
+    // Método suma, create a new Point adding this to p2.
     suma (p2) {
-       this.x += p2.x;
-       this.y += p2.y;
+      let pRes = new Point(this.x + p2.x, this.y + p2.y); 
+      return pRes;
+    }
+
+    isInside(center, radi) {
+       if ((this.x > (center.x - radi)) && (this.x < (center.x + radi))
+         && (this.y > (center.y - radi)) && (this.y < (center.y + radi)))  
+            return true;
+      return false;
     }
  }
  
@@ -21,6 +41,8 @@ class Point {
     dist () {
         return (Math.sqrt(this.x**2 + this.y**2));
     }
+    // invert this.x = cte / this.x
+   
  }
  
  
